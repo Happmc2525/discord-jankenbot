1fb8db5f1dc1f242a81eb240d2ac0c666cd545b3
Happmc2525 <60919012+Happmc2525@users.noreply.github.com>
2024-03-23 06:12:08 +0900

    Update janken.py

--- janken.py
+++ janken.py
@@ -2,26 +2,25 @@ import discord
 from discord import Option
 import random
 import time
+import asyncio
 
 TOKEN = 'ここにbotのトークンを入れる'
 DISCORD_SERVER_IDS = ここにサーバーのidを入れる
 
 client = discord.Bot(intents=discord.Intents.all())
 
-@client.slash_command(name="janken", description="じゃんけんコマンド", guild_ids=[DISCORD_SERVER_IDS])
+@bot.slash_command(name="janken", description="じゃんけんゲーム", guild_ids=[DISCORD_SERVER_IDS])
 async def on_message(message: discord.Message):
     await message.respond("最初はグー、じゃんけん")
-
     draw = "あいこ(そのままもう一度入力)"
     wn = 'kati.png'
     lst = 'make.png'
-
     def jankencheck(m):
         return (m.author == message.author) and (m.content in ["グー", "チョキ", "パー"])
-
     while True:
         jbot = random.choice(("グー", "チョキ", "パー"))
-        reply = await client.wait_for("message", check=jankencheck)
+        async with message.channel.typing():
+            reply = await bot.wait_for("message", check=jankencheck)
         if reply.content == jbot:
             jkekka = draw
             await message.channel.send(jbot)
@@ -33,7 +32,6 @@ async def on_message(message: discord.Message):
                     await message.channel.send("チョキ")
                     break
                 else:
-                    jkekka = lst
                     await message.channel.send("パー")
                     break
             elif reply.content == "チョキ":
